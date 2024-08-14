d5b87013c87f40488a2b84bc4c8474fafbc78c82
ideateGudy <azonubigoodnews@gmail.com>
2024-08-14 14:40:56 +0100

    change file structure

--- manifests/alert-manager/delete-alertmanager.sh
+++ manifests/alert-manager/delete-alertmanager.sh
@@ -5,16 +5,14 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Array of YAML files in the correct order
 yaml_files=(
-    "00-ingress-nginx.yml"
-    "02-issuer-prod.yaml"
-    "03-main-ingress.yaml"
+    "03-alertmanager-svc.yaml"
+    "02-alertmanager-dep.yaml"
+    "01-alertmanager-configmap.yaml"
+    "00-slack-hook-secret.yaml"
 )
 
-    # "01-cert-manager.crds.yaml"
 # Loop through YAML files and apply them
 for yaml_file in "${yaml_files[@]}"; do
-    kubectl apply -f "$SCRIPT_DIR/$yaml_file"
+    kubectl delete -f "$SCRIPT_DIR/$yaml_file"
 done
 
-ip-addr=kubectl get svc -n ingress-nginx
-echo "${ip-addr}"
